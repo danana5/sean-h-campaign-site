c857cd49d96c71629b576d03889e34f5193962b4
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-15 15:55:34 +0000

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer class="font-weight-medium" color="#ffa5ac" dark>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} —
+      <strong>Daniel Grace</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
