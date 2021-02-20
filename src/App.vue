f38be106921ce344058bb47828fb9f29ec704443
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-20 18:16:10 +0000

    formatting fixes and minor UI changes

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,28 @@
 <template>
-    <v-app>
-      <div>
-            <v-card
-                dark
-                color="#ffa5ac"
-                flat
-                height="400"
-            >
-                <v-container fill-height>
-                    <v-row
-                align="center"
-                justify="center"
-                >
-                  <v-card
-                  flat
-                  class="text-center"
-                  color="blue darken-4"
-                  >
-                      <h1 class="display-1 font-weight-thin mb-4">Vote Sean Hallahan #1</h1>
-                      <h4 class="subheading mb-4">For Clubs and Societies</h4>
-                  </v-card>
+<v-app>
+    <div>
+        <v-card dark color="#ffa5ac" flat height="400" tile>
+            <v-container fill-height>
+                <v-row align="center" justify="center">
+                    <v-card flat class="text-center" color="indigo darken-4">
+                        <v-container>
+                            <h1 class="display-1 font-weight-thin mb-4">Vote Sean Hallahan #1</h1>
+                            <h4 class="subheading mb-4">For Clubs and Societies</h4>
+                        </v-container>
+                    </v-card>
                 </v-row>
-                </v-container>
-            </v-card>          
+            </v-container>
+        </v-card>
         <v-flex id="About">
-            <About/>
+            <About />
         </v-flex>
         <v-flex id="Contact">
-            <Contact/>
+            <Contact />
         </v-flex>
-        <Footer/>
+        <Footer />
     </div>
 
-    </v-app>
+</v-app>
 </template>
 
 <script>
@@ -40,7 +30,7 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 export default {
-    components:{
+    components: {
         About,
         Footer,
         Contact
