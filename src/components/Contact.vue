ef76704467a746e953c47a5efa36ca24cf6612f0
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-20 18:15:46 +0000

    added seans links to contact list and formatting fixes

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,66 +1,59 @@
 <template>
-  <div>
+<div style="background: #1A237E">
     <v-container>
-      <div class="text-center">
-        <h1 class="display-2 font-weight-thin red--text text--lighten-3">Contact Me</h1>
-      </div>
-      <v-divider/>
+        <div class="text-center">
+            <h1 class="display-2 font-weight-thin red--text text--lighten-3">Contact Me</h1>
+        </div>
+        <v-divider dark />
         <v-layout wrap>
-          <v-flex wrap v-for="(contact, i) in contactLinks" :key="i">
-              <ContactList :contact="contact" />
-          </v-flex>
+            <v-flex wrap v-for="(contact, i) in contactLinks" :key="i">
+                <ContactList :contact="contact" />
+            </v-flex>
         </v-layout>
     </v-container>
-  </div>
+</div>
 </template>
 
 <script>
 import ContactList from "./ContactList"
 export default {
-  components: {
-    ContactList
-  },
-  data () {
-    return {
-      contactLinks: [
-        {
-          name: "Email",
-          value: "daniel.grace44@hotmail.com",
-          link: "mailto:daniel.grace44@hotmail.com",
-          icon: "mdi-email"
-        },
-        {
-          name: "LinkedIn",
-          value: "Daniel Grace",
-          link: "https://www.linkedin.com/in/daniel-grace-892963194/",
-          icon: "mdi-linkedin"
-        },
-        {
-          name: "Snapchat",
-          value: "danana_5",
-          link: "https://www.snapchat.com/l/en-gb/",
-          icon: "mdi-snapchat"
-        },
-        {
-          name: "Github",
-          value: "danana5",
-          link: "https://github.com/danana5",
-          icon: "mdi-github"
-        },
-        {
-          name: "Facebook",
-          value: "Daniel Grace",
-          link: "https://www.facebook.com/uzirthapa",
-          icon: "mdi-facebook"
-        },
-        {
-          name: "Instagram",
-          value: "danana5",
-          link: "https://www.instagram.com/danana_5/",
-          icon: "mdi-instagram"
+    components: {
+        ContactList
+    },
+    data() {
+        return {
+            contactLinks: [{
+                    name: "Email",
+                    value: "x18392221@student.ncirl.ie",
+                    link: "mailto:x18392221@student.ncirl.ie",
+                    icon: "mdi-email"
+                },
+                {
+                    name: "LinkedIn",
+                    value: "Sean Hallahan",
+                    link: "https://www.linkedin.com/in/sean-hallahan-sh20/",
+                    icon: "mdi-linkedin"
+                },
+                {
+                    name: "Snapchat",
+                    value: "danana_5",
+                    link: "https://www.snapchat.com/l/en-gb/",
+                    icon: "mdi-snapchat"
+                },
+                {
+                    name: "Facebook",
+                    value: "Sean Hallahan",
+                    link: "https://www.facebook.com/sean.hallahan.58",
+                    icon: "mdi-facebook"
+                },
+                {
+                    name: "Instagram",
+                    value: "seanh_cs",
+                    link: "https://www.instagram.com/seanh_cs/",
+                    icon: "mdi-instagram"
+                }
+            ]
         }
-      ]
     }
-  }
 }
 </script>
