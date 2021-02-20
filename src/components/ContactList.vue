ab82045d2276c944eb553a9ef24c5cb9a5c6e67f
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-20 18:14:32 +0000

    formatting fixes

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,27 +1,27 @@
 <template>
-        <v-list-item>
-          <v-list-item-icon class="pa-0 mr-2">
-            <v-btn icon :href="contact.link" target="_blank">
-              <v-icon>{{contact.icon}}</v-icon>
-            </v-btn>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{contact.value}}</v-list-item-title>
-            <v-list-item-subtitle>{{contact.name}}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+<v-list-item>
+    <v-list-item-icon class="pa-0 mr-2">
+        <v-btn icon :href="contact.link" target="_blank" class="white--text">
+            <v-icon>{{contact.icon}}</v-icon>
+        </v-btn>
+    </v-list-item-icon>
+    <v-list-item-content class="white--text">
+        <v-list-item-title>{{contact.value}}</v-list-item-title>
+        <v-list-item-subtitle class="white--text">{{contact.name}}</v-list-item-subtitle>
+    </v-list-item-content>
+</v-list-item>
 </template>
 
 <script>
 export default {
     name: "ContactList",
     props: {
-            contact : {
+        contact: {
             type: Object,
-            default: function() {
-                return{}
-            }
+            default: function () {
+                return {}
             }
         }
+    }
 }
 </script>
