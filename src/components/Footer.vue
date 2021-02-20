f90fb7d4995489e4c0d4643b7987a53e0b58eea3
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-20 18:34:39 +0000

    added link to me on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <v-footer class="font-weight-medium" color="#ffa5ac" dark>
     <v-col class="text-center" cols="12">
         {{ new Date().getFullYear() }} —
-        <strong>Daniel Grace</strong>
+        Designed by <strong><a style="text-decoration:none" href="https://www.linkedin.com/in/daniel-grace-892963194/">Daniel Grace</a></strong>
     </v-col>
 </v-footer>
 </template>
