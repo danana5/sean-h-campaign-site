f225b6f88f664122bf53fd6ac9370a56d68c0746
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-20 18:16:30 +0000

    added new images to carousel

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,47 +1,55 @@
 <template>
-  <div>
+<div style="background: #1A237E">
     <v-container class="text-center">
         <h1 class="display-2 font-weight-thin red--text text--lighten-3"> About Me</h1>
-        <v-divider/>
+        <v-divider dark />
         <v-card flat max-width="900" class="mx-auto" color="transparent">
-          <div class="body-1 blue-grey--text text--darken-1">Hello! I am Sean, I am a 21 year old lad from Swords in third year Computer Science at Trinity College Dublin. 
-              I've learned so much and I have gained many unique experiences but I've also been fortunate enough to meet many great pe++ople and characters whom I am lucky to 
-              call friends.<br><br>I am a front-end developer but with a strong interest also with full stack development. I have experience using React.js and Vue.js 
-              aswell as Python, Java and C.<br><br>Anyways, I played semi-proffesional football for Drogheda United in the League of Ireland. I am obsessed with music and several 
-              artists such as; Flume, deadmau5, 
-              Porter Robinson and Brockhampton. Film-wise, I love Christopher Nolan and nearly all of his films, I like Tarantino also and I'm a huge fan of Denis Villeneuve.
-          </div>
-          <div class="mt-8" width="100">
-          <v-carousel
-          cycle
-          interval=5000
-          >
-            <v-carousel-item
-            v-for="(item,i) in items"
-            :key="i"
-            :src="logo(item.src)"
-            ></v-carousel-item>
-          </v-carousel>
-        </div>
+            <div class="body-1 white--text">Hello! I am Sean, I am a 21 year old lad from Swords in third year Computer Science at Trinity College Dublin.
+                I've learned so much and I have gained many unique experiences but I've also been fortunate enough to meet many great people and characters whom I am lucky to
+                call friends.<br><br>I am a front-end developer but with a strong interest also with full stack development. I have experience using React.js and Vue.js
+                aswell as Python, Java and C.<br><br>Anyways, I played semi-proffesional football for Drogheda United in the League of Ireland. I am obsessed with music and several
+                artists such as; Flume, deadmau5,
+                Porter Robinson and Brockhampton. Film-wise, I love Christopher Nolan and nearly all of his films, I like Tarantino also and I'm a huge fan of Denis Villeneuve.
+            </div>
+            <div class="mt-8" width="100">
+                <v-carousel cycle interval=5000>
+                    <v-carousel-item v-for="(item,i) in items" :key="i" :src="logo(item.src)"></v-carousel-item>
+                </v-carousel>
+            </div>
         </v-card>
     </v-container>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  data () {
-      return {
-          items: [
-            {src: "slidetackle.jpg"},
-            {src: "team.jpg"}
-          ],
-      }
-  },
-  methods: {
-    logo(name) {
-      return require("../assets/" + name)
+    data() {
+        return {
+            items: [{
+                    src: "slidetackle.jpg"
+                },
+                {
+                    src: "team.jpg"
+                },
+                {
+                    src: "footballteam.png"
+                },
+                {
+                    src: "pubpic.png"
+                },
+                {
+                    src: "futsal.jpg"
+                },
+                {
+                    src: "cupwin.png"
+                },
+            ],
+        }
+    },
+    methods: {
+        logo(name) {
+            return require("../assets/" + name)
+        }
     }
-  }
 }
 </script>
